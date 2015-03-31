6a12d463d5bd07f5e57ba8467f2347cafa3e642a
Austin Meier <diveintotheword@att.net>
2015-03-31 12:25:43 -0500

    Pre-Assignment 12

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,4 @@
+
 public class BinaryTree 
 {	
 	//private Node root;
@@ -5,58 +6,73 @@ public class BinaryTree
 	private int payload;
 	private BinaryTree leftTree;
 	private BinaryTree rightTree;
+	private int depth;
 	
 	public BinaryTree()
+	{
+		this(0); //this is how we call our own constructor
+	}
+	
+	private BinaryTree(int depth)
 	{
 		//this.root = null;
 		this.isEmpty = true;
 		this.leftTree = null;
 		this.rightTree = null;
+		this.depth = depth;
 	}
+	
 	public boolean search(int value)
-	{	
-		if(isEmpty)
+	{
+		//return true if value is in the tree
+		//return false if value is not in the tree
+		if(this.isEmpty)
 		{
 			return false;
 		}
 		else
 		{
-			if(value == this.payload)
+			if(this.payload == value)
 			{
 				return true;
 			}
-			else if (value < this.payload)
+			else
 			{
-				if(this.leftTree == null)
+				if(value < payload)
 				{
-					return false;
+					//check the left
+					if(this.leftTree == null)
+					{
+						return false;
+					}
+					else
+					{
+						return this.leftTree.search(value);
+					}
 				}
 				else
 				{
-					return this.leftTree.search(value);
-				}
-			}
-			else if (value > this.payload)
-			{
-				if(this.rightTree == null)
-				{
-					return false;
-				}
-				else
-				{
-					return this.rightTree.search(value);
+					//check the right
+					if(this.rightTree == null)
+					{
+						return false;
+					}
+					else
+					{
+						return this.rightTree.search(value);
+					}
 				}
 			}
 		}
-		return false;
 	}
-	public void visitInOrder()
+	
+	private void visitInOrder()
 	{
 		if(this.leftTree != null)
 		{
 			this.leftTree.visitInOrder();
 		}
-		System.out.println(this.payload);
+		System.out.println(this.payload + " : " + this.depth);
 		if(this.rightTree != null)
 		{
 			this.rightTree.visitInOrder();
@@ -89,8 +105,9 @@ public class BinaryTree
 		System.out.println(this.payload);
 	}
 	
-	public void displayInOrder() 
+	public void displayInOrder()
 	{
+		System.out.println("**** In Order ****");
 		if(this.isEmpty)
 		{
 			System.out.println("Empty Tree");
@@ -113,6 +130,7 @@ public class BinaryTree
 	
 	public void displayPreOrder()
 	{
+		System.out.println("**** Pre Order ****");
 		if(this.isEmpty)
 		{
 			System.out.println("Empty Tree");
@@ -135,6 +153,7 @@ public class BinaryTree
 	
 	public void displayPostOrder()
 	{
+		System.out.println("**** Post Order ****");
 		if(this.isEmpty)
 		{
 			System.out.println("Empty Tree");
@@ -155,6 +174,43 @@ public class BinaryTree
 		*/
 	}
 	
+	private int getMaxDepth()
+	{
+		if(this.leftTree == null && this.rightTree == null)
+		{
+			return this.depth;
+		}
+		else if(this.leftTree == null)
+		{
+			return this.rightTree.getMaxDepth();
+		}
+		else if(this.rightTree == null)
+		{
+			return this.leftTree.getMaxDepth();
+		}
+		else
+		{
+			return Math.max(this.leftTree.getMaxDepth(), this.rightTree.getMaxDepth());
+		}
+	}
+	
+	public boolean isBalanced()
+	{
+		if(this.isEmpty)
+		{
+			return true;
+		}
+		else
+		{
+			//get the depth of the left
+			int currMaxLeftDepth = this.leftTree == null?0:this.leftTree.getMaxDepth(); //inline if statement see 54:15 in video csc 300 3/31/15 //boolean-expr?true-val:false-val
+			int currMaxRightDepth = this.rightTree == null?0:this.rightTree.getMaxDepth();
+			System.out.println("Max Left = " + currMaxLeftDepth);
+			System.out.println("Max Right = " + currMaxRightDepth);
+			return Math.abs(currMaxLeftDepth - currMaxRightDepth) <= 1;
+		}	
+	}
+	
 	public void add(int value)
 	{
 		if(this.isEmpty)
@@ -168,7 +224,7 @@ public class BinaryTree
 			{
 				if(this.leftTree == null)
 				{
-					this.leftTree = new BinaryTree();
+					this.leftTree = new BinaryTree(this.depth+1);	
 				}
 				this.leftTree.add(value);
 			}
@@ -176,7 +232,7 @@ public class BinaryTree
 			{
 				if(this.rightTree == null)
 				{
-					this.rightTree = new BinaryTree();
+					this.rightTree = new BinaryTree(this.depth+1);
 				}
 				this.rightTree.add(value);
 			}
